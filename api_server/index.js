fd51c3bf9aa9cd2e5d4a79c75ce053050ecb5fb1
dcuervo <dominickcuervo238@gmail.com>
2024-08-07 00:02:18 +0200

    working towards account creation

--- api_server/index.js
+++ api_server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const port = 5050;
 
 const knex = require("knex")(
@@ -17,6 +20,8 @@ const app = express();
   app.use(cors());
   // morgan, a logging tool
   app.use(morgan('tiny'));
+  // body-parser
+  app.use(bodyParser.json());
 
 
 //routes
@@ -34,6 +39,30 @@ app.get('/store', (req,res) => {
     res.status(301).send('Error retrieving the store.')
   });
 });
+
+app.get('/users', (req, res) => {
+  knex("users")
+    .then(data =>{
+      res.status(200).json(data);
+    })
+})
+
+app.post('/createAccount', async (req, res) => {
+  const { fname, lname, username, password} = req.body;
+  // checks to see if the username exist
+  let usernameCheck = await knex('users').select('*').where('username', username);
+  if(usernameCheck.length === 0){
+  const hashedPassword = await bcrypt.hashSync(password, 10);
+  await knex('users').insert({
+    first_name: fname,
+    last_name: lname,
+    username: username,
+    password: hashedPassword
+  })
+  } else {
+
+  }
+})
 
 //listen
 app.listen(port, ()=>{
--- inventory_management/src/createAccount.js
+++ inventory_management/src/createAccount.js
@@ -1,26 +1,71 @@
 import './App.css';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
 export default function Account() {
+  let [fname, setFname] = useState('');
+  let [lname, setLname] = useState('');
+  let [username, setUsername] = useState('');
+  let [password, setPassword] = useState('');
+
+
+
   const navigate = useNavigate();
+  const startVerifier = async (e) => {
+    e.preventDefault();
+    const passer = await Verifier(fname, lname, username, password);
+    handleResponse(passer)
+  };
+
+  const handleResponse = () => {
+    navigate("/");
+  }
 
   return (
     <div>
       <div class = "accountCreation">
         <p>First Name</p>
-          <input type='text' /><br/>
+          <input type= "text" minLength= "1" maxLength= "25" placeholder= " " value={fname} onChange={(e) => setFname(e.target.value)} required/><br/>
         <p>Last Name</p>
-          <input type='text' /><br/>
+          <input type= "text" minLength= "1" maxLength= "25" placeholder= " " value={lname} onChange={(e) => setLname(e.target.value)} required/><br/>
         <p>Username</p>
-          <input type='text' /><br/>
+          <input type= 'text' minLength= "1" maxLength= "25" placeholder= " " value={username} onChange={(e) => setUsername(e.target.value)} required /><br/>
         <p>Password</p>
-          <input type='text' /><br/>
+          <input type= "password" minLength= "8" maxLength= "16" placeholder= " " value={password} onChange={(e) => setPassword(e.target.value)} required/><br/>
         <p>SSN</p>
           <input type='text' minLength="10" maxLength = "10" require /><br/>
-        <button className="returnHome" onClick={() => navigate("/")}>Complete-Registration</button><br/>
+        <button className="returnHome" onClick={(e) => startVerifier(e)}>Complete-Registration</button><br/>
         <button className="returnHome" onClick={() => navigate("/")}>Leave-Creation</button><br/>
       </div>
     </div>
   )
 };
+
+async function Verifier(fname, lname, username, password){
+  try {
+    const response = await fetch('http://localhost:3000/createAccount', {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      body:JSON.stringify({
+        fname, lname, username, password
+      }),
+    });
+
+    if(!response.ok) {
+      throw new Error('Failure to register, giving the boot');
+    }
+
+    const data = await response.json();
+    console.log(data);
+    console.log('Registered, welcome to the Red Dragon')
+    return true;
+
+  } catch (error) {
+    console.error('Error during registration:', error);
+    return false;
+
+  }
+}
